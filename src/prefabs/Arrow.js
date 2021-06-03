5ce8dadd0479fdd4c44a6752bc83d6658b1d7776
Unlimited-Zone <lli142@ucsc.edu>
2021-06-03 13:12:12 +0800

    Revert "quick update to arrow sprite"
    This reverts commit 05513cdafee9b229f21d60854f55b66e22d9314c.

--- src/prefabs/Arrow.js
+++ src/prefabs/Arrow.js
@@ -1,6 +1,6 @@
 class Arrow extends Phaser.Physics.Arcade.Sprite {
     constructor (scene, x, y, dirX, dirY) {
-        super(scene, x, y, 'arrowSprite');
+        super(scene, x, y, 'slash');
         scene.add.existing(this);
         scene.physics.add.existing(this);
         let dx = this.x - this.scene.player.x;
--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -20,7 +20,7 @@ class Load extends Phaser.Scene {
         this.load.image('enemyShield', 'enemy_shield.png');
         this.load.image('enemySpear', 'enemy_spear.png');
         this.load.image('enemyBow', 'enemy_bow.png');
-        this.load.image('arrowSprite', 'arrow.png');
+        //this.load.image('arrow','arrow.png');
         
         // tile map stuff
         this.load.image('mappng','maptile32.png');
